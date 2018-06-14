61c5a5a78b876471a9f4dfdb668139255af5be2c
Stanley Salvatierra <stanlee321@gmail.com>
2018-06-14 19:34:02 -0400

    Post to AWS
    Created post to aws via Post request with the user token, id and plate

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -8,13 +8,20 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class PostService {
 	private user: firebase.User;
-	private apiurl = "https://2wktbyciic.execute-api.us-east-1.amazonaws.com/prod/LAMBDA3Posts"
+	private apiurl = "https://2wktbyciic.execute-api.us-east-1.amazonaws.com/prod/read-user"
 	constructor(private http: HttpClient) {
 	}
 
+
 	sendPost(data_to_post) {
-		console.log('SEND TO AWS', data_to_post)
-		let headers = new Headers({ 'Content-Type': 'application/json' });
-		return this.http.post(this.apiurl, JSON.stringify(data_to_post))
+		//let headers = new Headers({ 'Content-Type': 'application/json' });
+		let postData = new FormData();
+		postData.append('plate' , data_to_post.plate);
+    	postData.append('cellnumber' , data_to_post.cellnumber);
+    	postData.append('token' , data_to_post.token);
+    
+		console.log('SEND TO AWS', postData)
+		return this.http.post(this.apiurl, JSON.stringify(data_to_post), {
+											headers: { 'Content-Type': 'application/json' }}) ;
 	}
 }
